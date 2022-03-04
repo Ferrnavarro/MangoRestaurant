1857d45070020500cb2f4c4e1b4e217758edd7b1
Ferrnavarro <fer.maganavarro@gmail.com>
2022-03-04 13:54:40 -0600

    Create ProductAPIController

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,60 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
+    {
+        protected readonly ResponseDto _response;
+        private readonly IProductRepository _productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<ResponseDto> Get()
+        {
+            try
+            {
+                var productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;              
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ResponseDto> Get(int id)
+        {
+            try
+            {
+                var productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+            }
+
+            return _response;
+        }
+    }
+}
